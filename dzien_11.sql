7b15fcf19cdd7dc62a92d441bb7fe0208fbb8d41
Grzeborz <grzegorz.szyperek@gmail.com>
2020-01-15 21:46:50 +0100

    day 11 - functions

---
+++ dzien_11.sql
@@ -0,0 +1,110 @@
+﻿
+----dodawanie danych za pomocą procedury
+--create procedure addProduct(
+--				@productName nvarchar(20),
+--				@categoryName nvarchar(20)
+--       )
+--as
+--begin
+--	declare @categoryId int;
+--	set @categoryId =
+--	    (select c.categoryId from Categories c where c.CategoryName = @categoryName)
+--	if (@categoryId is null)
+--	begin
+--		print 'No category, inserting a new one'
+--		insert into Categories(CategoryName) values (@categoryName)
+--	end
+--	set @categoryId =
+--	    (select c.categoryId from Categories c where c.CategoryName = @categoryName)
+--	insert into Products (ProductName, CategoryID) values (@productName, @categoryId)
+--end
+
+--zadanko ---
+-- tabela ORDErs - usunąć zamówienia starsze niż podane - dwa argumenty - rok i miesiąc
+
+--create procedure removeOrders(
+--				@Year INT(4),
+--				@Month INT(2)
+--       )
+--as
+--begin
+--	if ((select orderID, YEAR(OrderDate), MONTH(OrderDate)
+--			from Orders 
+--				where  exists (select orderID, YEAR(OrderDate), MONTH(OrderDate)
+--								from Orders YEAR(OrderDate) < @Year and MONTH(OrderDate) < @Month))) is null)
+--		begin
+--			print 'No category, inserting a new one'
+--		end
+--	eLSE
+--		DELETE from Orders 
+--				where  YEAR(OrderDate) < @Year and MONTH(OrderDate) < @Month
+
+--end
+
+--exec removeOrders 2017 2
+--lub
+--exec removeOrders @Year = 2017 @Month=2
+
+--select orderID, YEAR(OrderDate), MONTH(OrderDate)
+--from Orders 
+--where  YEAR(OrderDate) < 2000 and MONTH(OrderDate) < 1
+
+--go
+--create procedure lowePriceForCategory
+--	(@categoryName
+
+
+---ZADANIE 3 ---
+--Tabele: Categories, Products, [Order Details]
+--Napisać procedurę która Obniży cenę UnitPrice o ilość procent przekazaną jako argument dla wszystkich produktów 
+--lub dla podanej kategorii produktów
+
+--drop procedure if exists ObnizycCeneOProcent
+--go
+--create procedure ObnizycCeneOProcent(@Procent nvarchar(3), @Kategoria int = null )
+--as
+--BeGIN
+--	print 'ObnizycCeneOProcent'
+--	print @Procent
+--	print @Kategoria
+--	if @Kategoria is null
+--		select (od.UnitPrice*@Procent/100) as UnitPricePoObnizce,od.UnitPrice, c.CategoryID
+--			from Categories c
+--				left join  Products p on c.CategoryID = p.CategoryID
+--				left join [Order Details] od on od.ProductID=p.ProductID
+--	else
+--		select (od.UnitPrice*@Procent/100) as UnitPricePoObnizce,od.UnitPrice, c.CategoryID
+--			from Categories c
+--				left join  Products p on c.CategoryID = p.CategoryID
+--				left join [Order Details] od on od.ProductID=p.ProductID
+--				where c.CategoryID = @Kategoria
+--END
+
+--exec ObnizycCeneOProcent @Procent = 50, @Kategoria = 2
+
+
+---ZADANIE 5---
+--Napisać procedurę która będzie obniżać cenę aż średnia cena będzie mniejsza od podanej jako argument
+drop procedure if exists ObnizycCeneSredniaOArgument
+go
+create procedure ObnizycCeneSredniaOArgument(@ArgumentObnizajacy float)
+as
+BeGIN
+	print 'ArgumentObnizajacy'
+	print @ArgumentObnizajacy
+	declare @Procent float = 1
+	declare @sredniaCena float = (select AVG(unitprice) from [Order Details])
+	while @sredniaCena >= @ArgumentObnizajacy
+		--print @sredniaCena
+
+		begin
+			print @Procent
+			set @sredniaCena = (select AVG(UnitPrice-(UnitPrice*@Procent/100)) from [Order Details])
+			print @sredniaCena
+			print @Procent
+			set @Procent = @Procent + 1
+		end
+		print @Procent
+END
+
+exec ObnizycCeneSredniaOArgument 10
---
+++ dzien_11_funkcje.sql
@@ -0,0 +1,58 @@
+﻿---FUNKCJA---
+
+--use Northwind
+--go
+--create function dodaj(@arg1 int, @arg2 int)
+--returns int
+--begin
+--	return @arg1 + @arg2;
+--end
+
+--select dbo.dodaj(1,1)
+
+--use Northwind
+--go
+--create function ponozPrzez2(@arg float)
+--returns float
+--begin
+--	return @arg*2;
+--end
+
+--select dbo.ponozPrzez2(2)
+
+--select UnitPrice, dbo.ponozPrzez2(2) from Products
+
+--Stwórz funkcję obliczającą cenę towaru--
+--use Northwind
+--go
+--create function dbo.podajCeneTowaru(@unitPrice money, @quantity int, @discount float)
+--returns money
+--begin
+--	return (@unitPrice * @quantity)*(1-@discount)
+--end
+
+--go
+--select dbo.podajCeneTowaru(unitprice, Quantity, Discount), UnitPrice, Quantity, Discount from [Order Details]
+
+---ZADANKO ---
+--1. Należy napisać funkcję, która będzie usuwać z napisu który reprezentuje telefon niepotrzebne znaki.
+--TRANSLARE, REPLACE
+--2. Tabelka Customers. Należy nową funkcję użyć do zaktualizowania kolumny Phone.
+
+use Northwind
+go
+drop function if exists bezZnakowSpecjalnych
+go
+create function bezZnakowSpecjalnych(@telefon nvarchar(100))
+returns nvarchar(100)
+as
+begin
+	return replace(translate(@telefon, '!@#$%^&*()_+.-{}|:"?><'' ','XXXXXXXXXXXXXXXXXXXXXXXX'),'X', '')
+end
+
+--select dbo.bezZnakowSpecjalnych('$#123456#$%6 :>?''78')
+
+select dbo.bezZnakowSpecjalnych(fax), fax from Customers
+
+--jeśli teraz chciałbym updateować kolumne to mogę--
+--update Customers set Phone = dbo.bezZnakowSpecjalnych(Phone)
